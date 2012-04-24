0916690fdc094ebae5843932eccf0552f6f91eb4
Zearin <zearin@gonk.net>
2012-04-24 10:45:32 -0400

    Made “The Light Source” use external JS file (and updated external JS file to working code ;)

--- source.js
+++ source.js
@@ -1,70 +1,73 @@
 function objectPosition(obj) {
-	var curleft = 0;
-	var curtop = 0;
-	if (obj.offsetParent) {
-		do {
-			curleft += obj.offsetLeft;
-			curtop += obj.offsetTop;
-		} while (obj = obj.offsetParent);
-	}
-	return [curleft,curtop];
+    var curleft = 0;
+    var curtop  = 0;
+    if (obj.offsetParent) {
+        do {
+            curleft += obj.offsetLeft;
+            curtop  += obj.offsetTop;
+        } while (obj = obj.offsetParent);
+    }
+    return [curleft,curtop];
 }
-
-window.box          = document.getElementsByClassName('source')[0];
-window.boxshadow    = box.getElementsByTagName('span')[0];
-window.boxLeft      = objectPosition(box)[0];
-window.boxTop       = objectPosition(box)[1];
-window.canvasWidth  = window.innerWidth;
-window.canvasHeight = window.innerHeight; 
-window.timer        = 0;
 
 
 document.body.addEventListener('mousemove', function(movement){
+    
+    box                 = document.getElementsByClassName('source')[0];
+    boxshadow           = box.getElementsByTagName('span')[0];
+    boxLeft             = objectPosition(box)[0];
+    boxTop              = objectPosition(box)[1];
+    window.canvasWidth  = window.innerWidth;
+    window.canvasHeight = window.innerHeight;
+    window.timer        = 0;
+    
+    function moveShadows() {
 
-	function moveShadows(){
-		var mouseLeft           = movement.pageX,
-			mouseTop            = movement.pageY,
-			distanceFromBoxX    = mouseLeft - boxLeft - 75,
-			distanceFromBoxY    = mouseTop  - boxTop  - 75,
-			normalizedDistanceX = (distanceFromBoxX / canvasWidth),
-			normalizedDistanceY = (distanceFromBoxY / canvasHeight),
+        var mouseLeft           = movement.pageX,
+            mouseTop            = movement.pageY,
+            distanceFromBoxX    = mouseLeft - boxLeft - 75,
+            distanceFromBoxY    = mouseTop  - boxTop  - 75,
+            normalizedDistanceX = (distanceFromBoxX / canvasWidth),
+            normalizedDistanceY = (distanceFromBoxY / canvasHeight),
 
-			//shadow
-			shadowLeftMultiplier    = -40,
-			shadowTopMultiplier     = -10,
-			shadowTopOffset         = 20,
-			newShadowLeft           = normalizedDistanceX * shadowLeftMultiplier + 'px',
-			newShadowTop            = normalizedDistanceY * shadowTopMultiplier + shadowTopOffset + 'px',
+            //shadow
+            shadowLeftMultiplier= -40,
+            shadowTopMultiplier = -10,
+            shadowTopOffset     = 20,
+            newShadowLeft       = normalizedDistanceX * shadowLeftMultiplier + 'px',
+            newShadowTop        = normalizedDistanceY * shadowTopMultiplier  + shadowTopOffset + 'px',
 
-			// gradient
-			gradientLeftMultiplier  = 120,
-			gradientTopMultiplier   = 80,
-			gradientLeftOffset      = 75,
-			gradientTopOffset       = 20,
-			newGradientLeft         = normalizedDistanceX * gradientLeftMultiplier + gradientLeftOffset,
-			newGradientTop          = normalizedDistanceY * gradientTopMultiplier + gradientTopOffset,
-			gradient                = '-webkit-gradient(';
-		
-		gradient += 'radial, ';
-		gradient += newGradientLeft + ' ' + newGradientTop + ', ';
-		gradient += '75, ';
-		gradient += newGradientLeft + ' ' + newGradientTop + ', ';
-		gradient += '40, ';
-		gradient += 'from(#444), ';
-		gradient += 'to(#777)';
-		gradient += ')';
+            // gradient
+            gradientLeftMultiplier  = 120,
+            gradientTopMultiplier   = 80,
+            gradientLeftOffset      = 75,
+            gradientTopOffset       = 20,
+            newGradientLeft         = normalizedDistanceX * gradientLeftMultiplier + gradientLeftOffset,
+            newGradientTop          = normalizedDistanceY * gradientTopMultiplier  + gradientTopOffset,
+            gradient                = '-webkit-gradient(';
 
-		if(distanceFromBoxY !== 0 && distanceFromBoxX !== 0){
-			window.boxshadow.style.left = newShadowLeft;
-			window.boxshadow.style.top  = newShadowTop;
-			window.box.style.background = gradient;
-		}
+        gradient += 'radial, ';
+        gradient += newGradientLeft + ' ' + newGradientTop + ', ';
+        gradient += '75, ';
+        gradient += newGradientLeft + ' ' + newGradientTop + ', ';
+        gradient += '40, ';
+        gradient += 'from(#444), ';
+        gradient += 'to(#777)';
+        gradient += ')';
 
-	}
+        if (distanceFromBoxY != 0 && distanceFromBoxX != 0) {
+            if (boxshadow.hasOwnProperty('style') ) {
+                boxshadow.style.left = newShadowLeft;
+                boxshadow.style.top  = newShadowTop;
+                box.style.background = gradient;
+            }
+        }
 
-	if (timer) {
-		clearTimeout(timer);
-	}
-	
-	timer = setTimeout(moveShadows, 10);
+    }
+
+    if (timer) {
+        clearTimeout(timer);
+    }
+    
+    timer = setTimeout(moveShadows, 10);
 });
